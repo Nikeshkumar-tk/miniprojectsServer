3e4ceb76d806362708eed5c3e7871d4e135b0294
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2022-07-10 12:21:05 +0530

    models completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require("express")
+const app=express()
+const dotenv=require("dotenv")
+const mongoose=require("mongoose")
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL).then(console.log("DATABASE CONECTED")).catch((err)=>console.log(err))
+
+app.listen(5000,()=>console.log("server started"));
